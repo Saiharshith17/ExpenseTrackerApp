1a851ff478f080c9bb3374aefa6556412e8c4346
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-25 15:33:26 +0530

    commited

--- app/src/main/java/org/example/service/RefreshTokenService.java
+++ app/src/main/java/org/example/service/RefreshTokenService.java
@@ -1,5 +1,19 @@
 package org.example.service;
 
+import org.example.entities.RefreshToken;
+import org.example.entities.UserInfo;
+import org.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RefreshTokenService {
-    //dfimfe
+
+    @Autowired
+    UserRepository userRepository;
+
+    public RefreshToken createRefreshToken(String username){
+        UserInfo userInfoExtracted=userRepository.findByUsername(username);
+
+    }
 }
