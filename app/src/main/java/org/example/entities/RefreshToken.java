1328b485785c329ee8142e516a0b409269c62e61
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-25 16:08:32 +0530

    added till RefreshTokenService

--- app/src/main/java/org/example/entities/RefreshToken.java
+++ app/src/main/java/org/example/entities/RefreshToken.java
@@ -2,14 +2,12 @@ package org.example.entities;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.util.UriBuilder;
 
 import java.time.Instant;
 
@@ -22,9 +20,14 @@ import java.time.Instant;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @Table(name="tokens")
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String token;
     private Instant expiryDate;
+
+
 
     @OneToOne
     @JoinColumn(name="id",referencedColumnName = "user_id")
